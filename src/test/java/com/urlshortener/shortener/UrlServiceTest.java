29ef27404a71a051a79db304dc24bfbe91f97330
Dahua Yi <dahuayi@berkeley.edu>
2025-05-22 21:21:11 -0700

    test

--- src/test/java/com/urlshortener/shortener/UrlServiceTest.java
+++ src/test/java/com/urlshortener/shortener/UrlServiceTest.java
@@ -3,6 +3,7 @@ package com.urlshortener.shortener;
 import org.junit.jupiter.api.Test;
 import java.lang.reflect.Method;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashSet;
 
 public class UrlServiceTest {
     @Test
@@ -14,5 +15,39 @@ public class UrlServiceTest {
         method.setAccessible(true);
         String code = (String) method.invoke(service);
         assertEquals(6, code.length());
+    }
+
+    @Test
+    void testShortCodeUniqueness() throws Exception {
+        var serviceClass = Class.forName("com.urlshortener.shortener.service.UrlService");
+        Object service = serviceClass.getDeclaredConstructor().newInstance();
+        Method method = serviceClass.getDeclaredMethod("generateShortCode");
+        method.setAccessible(true);
+        HashSet<String> codes = new HashSet<>();
+        for (int i = 0; i < 1000; i++) {
+            String code = (String) method.invoke(service);
+            assertFalse(codes.contains(code), "Duplicate code found: " + code);
+            codes.add(code);
+        }
+    }
+
+    @Test
+    void testIsValidUrlTrue() throws Exception {
+        var serviceClass = Class.forName("com.urlshortener.shortener.service.UrlService");
+        Object service = serviceClass.getDeclaredConstructor().newInstance();
+        Method method = serviceClass.getDeclaredMethod("isValidUrl", String.class);
+        method.setAccessible(true);
+        assertTrue((Boolean) method.invoke(service, "https://example.com"));
+        assertTrue((Boolean) method.invoke(service, "http://localhost:8080/test"));
+    }
+
+    @Test
+    void testIsValidUrlFalse() throws Exception {
+        var serviceClass = Class.forName("com.urlshortener.shortener.service.UrlService");
+        Object service = serviceClass.getDeclaredConstructor().newInstance();
+        Method method = serviceClass.getDeclaredMethod("isValidUrl", String.class);
+        method.setAccessible(true);
+        assertFalse((Boolean) method.invoke(service, "not_a_url"));
+        assertFalse((Boolean) method.invoke(service, "htp:/bad-url"));
     }
 } 
