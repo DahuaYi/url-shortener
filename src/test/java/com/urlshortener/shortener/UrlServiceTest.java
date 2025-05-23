6f032b14a166d3bd71a2160577750596b1de458f
Dahua Yi <dahuayi@berkeley.edu>
2025-05-22 20:54:51 -0700

    test

--- src/test/java/com/urlshortener/shortener/UrlServiceTest.java
+++ src/test/java/com/urlshortener/shortener/UrlServiceTest.java
@@ -1,14 +1,18 @@
 package com.urlshortener.shortener;
 
-import com.urlshortener.shortener.service.UrlService;
 import org.junit.jupiter.api.Test;
+import java.lang.reflect.Method;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UrlServiceTest {
     @Test
-    void testGenerateShortCodeLength() {
-        UrlService service = new UrlService();
-        String code = service.shortenUrl("https://example.com").getShortCode();
+    void testGenerateShortCodeLength() throws Exception {
+        // Use reflection to test the private generateShortCode method
+        var serviceClass = Class.forName("com.urlshortener.shortener.service.UrlService");
+        Object service = serviceClass.getDeclaredConstructor().newInstance();
+        Method method = serviceClass.getDeclaredMethod("generateShortCode");
+        method.setAccessible(true);
+        String code = (String) method.invoke(service);
         assertEquals(6, code.length());
     }
 } 
